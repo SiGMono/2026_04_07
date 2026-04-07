0ca976920ba7693e51c5c360f1c24cd50937fc93
SiGMono <szymala777@gmail.com>
2026-04-07 14:09:26 +0200

    1.0

--- app/odtwarzaczeAdapter.cpp
+++ app/odtwarzaczeAdapter.cpp
@@ -16,9 +16,11 @@ class Katarynka{
     public:
         void krecKorbka(){
             korbka = true;
+            cout<<"Kreci korbka"<<endl;
         }
         void zatrzymajKrecenie(){
             korbka = false;
+            cout<<"Przestaje krecic korbka"<<endl;
         }
 };
 
@@ -51,21 +53,60 @@ class CDPlayer: public Odtwarzacz{
         }
 };
 
-class MP3Player{
-
+class MP3Player: public Odtwarzacz{
+    private:
+        string plik = "muzyka_z_MP3";
+    public:
+        MP3Player(string plik) : plik(plik){}
+        void odtwarzaj() override{
+            cout<<"Odtwarzam plik: "<<plik<<endl;
+        }
+        void zatrzymaj() override{
+            cout<<"Zatrzymuje plik: "<<plik<<endl;
+        }
 };
 
 int main(int argc, char const *argv[])
 {
     int decyzja;
     Odtwarzacz *user = NULL;
-    Katarynka *Katarynka = new Katarynka();
+    Katarynka *katarynka = new Katarynka();
     CDPlayer *cdPlayer = new CDPlayer("muzyka_z_CD");
+    MP3Player *mp3Player = new MP3Player("muzyka_z_MP3");
+    KatarynkaAdapter *katarynkaAdapter = new KatarynkaAdapter(katarynka);
 
     do{
-        cout<<"1. Odtwarzacz CD \n 2. Katarynka \n 3. Odtwarzacz MP3 \n 0. Wyjdz"<<endl;
+        cout<<" 1. Odtwarzacz CD \n 2. Katarynka \n 3. Odtwarzacz MP3 \n 5. Graj \n 6. Zatrzymaj \n 0. Wyjdz"<<endl;
+        cin>>decyzja;
+        switch(decyzja){
+            case 1:
+                user = cdPlayer;
+                cout<<"Wybrano CD"<<endl;
+                break;
+            case 2:
+                user = katarynkaAdapter;
+                cout<<"Wybrano Katarynke"<<endl;
+                break;
+            case 3:
+                user = mp3Player;
+                cout<<"Wybrano MP3"<<endl;
+                break;
+            case 5:
+                if(user != NULL){
+                    user->odtwarzaj();
+                }
+                break;
+            case 6:
+                if(user != NULL){
+                    user->zatrzymaj();
+                }
+                break;
+            case 0:
+                cout<<"Wyjscie z programu"<<endl;
+                break;
+        }
     }while(decyzja != 0);
 
-    delete(Katarynka,cdPlayer);
+    delete(katarynka, cdPlayer, katarynkaAdapter, mp3Player);
     return 0;
 }
